3011279dc23cc1ab70f68e7b4bddc40225a85d8e
pierre <pierre@squarra.de>
2024-05-27 23:37:09 +0200

    added tracking example

--- models/ultralytics/track.py
+++ models/ultralytics/track.py
@@ -3,10 +3,10 @@ import argparse
 def main(model_path):
     from ultralytics import YOLO
     model = YOLO(model_path)
-    model.predict("https://ultralytics.com/images/bus.jpg", save=True, imgsz=320, conf=0.5)
+    model.track("person-bicycle-car-detection.mp4", save=True)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Make a prediction on an image")
+    parser = argparse.ArgumentParser(description="Use ultralytics tracking functionality")
     parser.add_argument("model_path", help="Path to the model weights file.")
     args = parser.parse_args()
     main(args.model_path)
