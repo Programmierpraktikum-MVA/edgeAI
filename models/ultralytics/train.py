bb8809038548664b5593b529b20cded33c544b53
pierre squarra <pierre@squarra.de>
2024-05-27 19:39:49 +0200

    added wandb integration

--- models/ultralytics/train.py
+++ models/ultralytics/train.py
@@ -1,13 +1,23 @@
 import argparse
 
-def main(data_path, num_epochs):
+def main(data_path, num_epochs, wandb):
     from ultralytics import YOLO
     model = YOLO("yolov8n.pt")
+    
+    if wandb:
+        import wandb
+        from wandb.integration.ultralytics import add_wandb_callback
+        add_wandb_callback(model, enable_model_checkpointing=True)
+    
     model.train(data=data_path, epochs=num_epochs)
+    
+    if wandb:
+        wandb.finish()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a YOLO model on a given dataset.")
     parser.add_argument("data_path", help="Path to the configuration YAML file.")
     parser.add_argument("-e", "--epochs", type=int, default=1, help="Number of epochs to train the model for (default is 1).")
+    parser.add_argument("--wandb", action="store_true", help="Flag to enable tracking with wandb")
     args = parser.parse_args()
-    main(args.data_path, args.epochs)
+    main(args.data_path, args.epochs, args.wandb)
