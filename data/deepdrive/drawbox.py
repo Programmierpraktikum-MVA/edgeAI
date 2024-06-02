7e40e5dc0add8343198ec4e8f9965860fee84c49
NeoStar44 <167795016+NeoStar44@users.noreply.github.com>
2024-06-02 17:01:47 +0200

    Update drawbox.py

--- data/deepdrive/drawbox.py
+++ data/deepdrive/drawbox.py
@@ -1,18 +1,16 @@
 import cv2
 import os
 
-class_dict = {
-    "pedestrian": 0,
-    "rider": 1,
-    "car": 2,
-    "truck": 3,
-    "bus": 4,
-    "train": 5,
-    "motorcycle": 6,
-    "bicycle": 7,
-    "traffic light": 8,
-    "traffic sign": 9,
-}
+class_dict = ["pedestrian",
+    "rider",
+    "car",
+    "truck",
+    "bus",
+    "train",
+    "motorcycle",
+    "bicycle",
+    "traffic light",
+    "traffic sign"]
 
 # example jpg: images/train/0a0a0b1a-7c39d841.jpg
 path = input("images/val/.jpg oder images/train/.jpg\n")
@@ -23,7 +21,6 @@ head, descriptionfile = os.path.split(path)
 descriptionfile = descriptionfile[:-4] + ".txt"
 txtpath = "labels" + head[6:] + f"/{descriptionfile}"
 
-# read txt-file line by line to draw the boxes
 with open(txtpath) as f:
     for line in f:
         label, x_center, y_center, width, height = line.split(" ")
@@ -35,10 +32,9 @@ with open(txtpath) as f:
         x2 = int((x_center + width/2)*1280)
         y1 = int((y_center - height/2)*720)
         y2 = int((y_center + height/2)*720)
-        print(x1,x2,y1,y2)
+    
         cv2.rectangle(image, (x1,y1),(x2,y2), (0,255,0),1)
-        cv2.putText(image, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
-
+        cv2.putText(image, class_dict[int(label)], (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
 
 cv2.imshow(f"{path}", image)
 cv2.waitKey(0)
