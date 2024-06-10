bcc40c941c7e0fea67413574725db28eda8cffaf
|Marix| <Miguel200184@Yahoo.de>
2024-06-10 19:25:11 +0200

    test.py delete

--- test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print(f"CUDA is available. Your GPU: {torch.cuda.get_device_name(0)}")
-else:
-    print("CUDA is not available. Please check your installation.")
